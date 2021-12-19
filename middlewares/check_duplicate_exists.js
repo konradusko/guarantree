a48894a9f7d5fb96fe4fa93d6b028168efa0c1cd
Konrad Bolesławski <42543331+konradusko@users.noreply.github.com>
2021-12-19 10:32:05 +0100

    Delete middlewares directory

--- middlewares/check_duplicate_exists.js
+++
@@ -1,7 +0,0 @@
-const check_duplicate_exists = (req,res,next)=>{
-    const values = Object.keys(req.body)
-    if(new Set(values).size !== values.length)
-    return res.json({message:'Istnieja duplikaty wartosci.'})
-    next()
-}
-export{check_duplicate_exists}
